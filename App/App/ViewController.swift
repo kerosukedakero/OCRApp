1a4697f567ba8184a2ae9a3b8fd813776e1a56dc
yuuki00682200 <yukitakatsu6468@gmail.com>
2022-08-06 18:03:20 +0900

    app commit

---
+++ App/App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App
+//
+//  Created by 髙津悠樹 on 2022/08/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
