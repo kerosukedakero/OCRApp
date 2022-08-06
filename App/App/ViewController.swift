b5e696a0760efb69c3abb002c3096450738cc07a
yuuki00682200 <yukitakatsu6468@gmail.com>
2022-08-06 18:04:59 +0900

    app testcommit

--- App/App/ViewController.swift
+++ App/App/ViewController.swift
@@ -8,10 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var ImageView: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    @IBAction func uploadButton() {
+        
     }
 
 
